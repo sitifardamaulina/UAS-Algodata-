15ecc9dcaca3a425a9cde234ebad3c37e11212ad
sitifardamaulina <80209705+sitifardamaulina@users.noreply.github.com>
2021-07-11 22:07:46 +0700

    Add files via upload

---
+++ menyisipkanKata1.java
@@ -0,0 +1,14 @@
+
+public class menyisipkanKata1 {
+
+    public static void main(String[] args) {
+     
+        StringBuffer data = new StringBuffer("Saya sedang belajar struktur data");
+        
+        data.insert(11," pergi ");
+        
+        
+        System.out.println(data);
+    }
+    
+}
